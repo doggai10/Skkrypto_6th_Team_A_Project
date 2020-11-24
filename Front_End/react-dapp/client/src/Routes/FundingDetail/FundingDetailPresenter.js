e4ccde66db842f3d76c2935a0cd30d72c25d1cb0
이희제 <holim1226@gmail.com>
2020-11-25 03:29:38 +0900

    add funding_detail

--- Front_End/react-dapp/client/src/Routes/FundingDetail/FundingDetailPresenter.js
+++ Front_End/react-dapp/client/src/Routes/FundingDetail/FundingDetailPresenter.js
@@ -1,1 +1,13 @@
-export default () => "Detail";
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+    margin: 10px 10px;
+    font-size: 40px;
+`;
+
+const FundingDetailPresenter = () => {
+    return <Button>펀딩하기</Button>;
+};
+
+export default FundingDetailPresenter;
